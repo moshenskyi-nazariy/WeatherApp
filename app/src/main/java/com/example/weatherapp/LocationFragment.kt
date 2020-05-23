41914c5cacf6f3b123dcb9519585cd4ae9d0d015
Nazarii Moshenskyi <nmosh@softserveinc.com>
2020-05-23 18:48:41 +0300

    Added location screen

---
+++ app/src/main/java/com/example/weatherapp/LocationFragment.kt
@@ -0,0 +1,120 @@
+package com.example.weatherapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class LocationFragment : Fragment() {
+    private val locationRequestCode = 1000
+
+    private lateinit var cityNameTextView: TextView
+    private lateinit var loadingBar: ProgressBar
+
+    private lateinit var viewModel: LocationViewModel
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.location_fragment, container, false)
+        initUi(root)
+
+        return root
+    }
+
+    private fun initUi(root: View) {
+        cityNameTextView = root.findViewById(R.id.city_name_tv)
+        loadingBar = root.findViewById(R.id.loading_bar)
+        loadingBar.visibility = VISIBLE
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            && checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            requestLocationPermissions()
+        } else {
+            lookupLocation()
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            locationRequestCode
+        )
+    }
+
+    private fun checkPermission(permission: String): Boolean {
+        return checkSelfPermission(
+            activity!!,
+            permission
+        ) != PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == locationRequestCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                lookupLocation()
+            } else {
+                Toast.makeText(activity!!, "Permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun lookupLocation() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener(activity!!, this::onLocationUpdateSuccess)
+            .addOnFailureListener(this::onLocationUpdateFailure)
+    }
+
+    private fun onLocationUpdateFailure(exception: Exception) {
+        loadingBar.visibility = GONE
+
+        activity?.let { context ->
+            Toast.makeText(context, exception.message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun onLocationUpdateSuccess(location: Location) {
+        loadingBar.visibility = GONE
+
+        val cityList = Geocoder(activity).getFromLocation(location.latitude, location.longitude, 1)
+        if (cityList.isNotEmpty()) {
+            cityNameTextView.text = cityList[0].locality
+        }
+    }
+
+    companion object {
+        fun newInstance() = LocationFragment()
+    }
+
+}
